3cc39d1a28e286b632a75671362f3b8b14b70b58
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2023-09-10 22:57:43 +0530

    2nd Commit

--- src/main/java/com/sarvagnya/Main.java
+++ src/main/java/com/sarvagnya/Main.java
@@ -1,8 +1,6 @@
 package com.sarvagnya;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
@@ -23,7 +21,7 @@ public class Main {
             int index = 0, length = children.size();
             while (index < length) {
                 Node leftChild = children.get(index);
-                Node rightChild = null;
+                Node rightChild;
 
                 if ((index + 1) < length) {
                     rightChild = children.get(index + 1);
@@ -41,48 +39,35 @@ public class Main {
         return children.get(0);
     }
 
-    private static void printLevelOrderTraversal(Node root) {
-        if (root == null) {
-            return;
+    private static void display(Node node, int level) {
+        if (node != null) {
+            display(node.getRight(), level + 1);
+            if (level != 0) {
+                for (int i = 0; i < level - 1; ++i)
+                    System.out.print("|\t");
+                System.out.println("|----" + node.getHash().substring(0, 15));
+            } else
+                System.out.println(node.getHash().substring(0, 15));
+            display(node.getLeft(), level + 1);
         }
-
-        if ((root.getLeft() == null && root.getRight() == null)) {
-            System.out.println(root.getHash());
-        }
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        queue.add(null);
-
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (node != null) {
-                System.out.println(node.getHash());
-            } else {
-                System.out.println();
-                if (!queue.isEmpty()) {
-                    queue.add(null);
-                }
-            }
-
-            if (node != null && node.getLeft() != null) {
-                queue.add(node.getLeft());
-            }
-
-            if (node != null && node.getRight() != null) {
-                queue.add(node.getRight());
-            }
-
-        }
-
     }
 
     public static void main(String[] args) {
         ArrayList<String> dataBlocks = new ArrayList<>();
-        dataBlocks.add("Captain America");
-        dataBlocks.add("Iron Man");
-        dataBlocks.add("God of thunder");
-        dataBlocks.add("Doctor strange");
+
+        // The grades for the 5 subjects are given in the transaction data
+        dataBlocks.add("Tx(1, VJTI, Sarvagnya, AA AB BB BC CC)");
+        dataBlocks.add("Tx(2, VJTI, Sarvagnya, AA AB AA AB BC)");
+        dataBlocks.add("Tx(3, VJTI, Sarvagnya, CC AB AA BB AB)");
+        dataBlocks.add("Tx(4, VJTI, Sarvagnya, BB AA AB BB BC)");
+        dataBlocks.add("Tx(5, VJTI, Sarvagnya, BC CC AA AA AB)");
+        dataBlocks.add("Tx(6, VJTI, Sarvagnya, AA BB CD AB BB)");
+        dataBlocks.add("Tx(7, VJTI, Sarvagnya, AB CC AA BB BB)");
+        dataBlocks.add("Tx(8, VJTI, Sarvagnya, AA BB AB AB CC)");
+        dataBlocks.add("Tx(9, VJTI, Sarvagnya, BC BC AA AB AA)");
+        dataBlocks.add("Tx(10, VJTI, Sarvagnya, AA BC BB AB CC)");
+
         Node root = generateTree(dataBlocks);
-        printLevelOrderTraversal(root);
+        display(root, 0);
     }
 }
